4b25ead44e049f107f6012b1ddf58a19dbb120f6
RaulBarbaRojas <barbarojasraul@gmail.com>
2026-02-14 13:29:08 +0100

    feat: added tracking of params and operations
    In order to calculate the gradient of a parameter
    within an expression, we need to keep track
    of how the expression was created, including
    the operations that defined the parameters (if
    any) and the parameters that led to the creation
    of new parameters.

--- src/tinygrad/core/parameter.py
+++ src/tinygrad/core/parameter.py
@@ -1,20 +1,39 @@
 """Implements a parameter with automatic gradient tracking.
 """
 
+from enum import Enum
 from typing import Self
+
+
+class Operation(str, Enum):
+    """An enumeration that showcases the operations that can lead to the
+    creation of new parameters (used for gradient tracking and calculation).
+    """
+
+    MANUAL_CREATION = ''
+    SUM: str = '+'
+    MUL: str = '*'
 
 
 class Parameter:
     """A parameter with automatic gradient tracking.
     """
 
-    def __init__(self, value: int | float | Self) -> None:
+    def __init__(self, value: int | float | Self,
+                 _prev: tuple[Self] | None = None,
+                 _op: str | Operation = Operation.MANUAL_CREATION) -> None:
         """Creates a new `Parameter` instance.
 
         :param value: The value of the parameter (or a parameter).
+        :param _prev: The previous parameters that led to the creation of
+        the parameter (if any), defaults to None
+        :param _op: The operation that led to the creation of the parameter
+        (if any), defaults to `Operation.MANUAL_CREATION`
         """
         value = value.value if isinstance(value, Parameter) else value
         self.value = value
+        self._prev = set(_prev if _prev is not None else tuple())
+        self._op = _op
 
     def __add__(self, param: int | float | Self) -> Self:
         """Adds a given parameter to the current object.
@@ -22,7 +41,9 @@ class Parameter:
         :param param: The parameter to be added.
         :return: A new parameter with the value of the sum.
         """
-        return Parameter(self.value + Parameter(param).value)
+        out_param = Parameter(self.value + Parameter(param).value,
+                              _prev=(self, param), _op=Operation.SUM)
+        return out_param
 
     def __mul__(self, param: int | float | Self) -> Self:
         """Multiplies a given parameter by the given object.
@@ -30,7 +51,9 @@ class Parameter:
         :param param: The parameter to be multiplied with self.
         :return: A new parameter with the product result.
         """
-        return Parameter(Parameter(param).value * self.value)
+        out_param = Parameter(Parameter(param).value * self.value,
+                              _prev=(self, param), _op=Operation.MUL)
+        return out_param
 
     def __repr__(self) -> str:
         """Provides a string representation of the parameter object.
