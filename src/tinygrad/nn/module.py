323ad27c3d305bacbce38563e31ccbcdc2017ba1
RaulBarbaRojas <barbarojasraul@gmail.com>
2026-02-22 12:29:34 +0100

    feat: added nn.Module to tinygrad
    The tinygrad package now has a generic nn.Module class that can be
    extended to implement neural networks.

---
+++ src/tinygrad/nn/module.py
@@ -0,0 +1,59 @@
+"""Implements the most basic structure to define tinygrad nnets.
+"""
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+from ..core import Parameter
+
+
+class Module(ABC):
+    """Defines a nnet in tinygrad.
+    """
+
+    def __init__(self) -> None:
+        """Creates a tinygrad neural network instance.
+        """
+        self._params: dict[str, list[Parameter]] = {}
+
+    @property
+    def parameters(self) -> list[Parameter]:
+        """Retrieves the parameters of the defined neural network.
+        """
+        return [param
+                for module_params in self._params.values()
+                for param in module_params]
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        """Sets a given value as an attribute of the module.
+
+        :param name: The name of the attribute.
+        :param value: The value of the attribute.
+        """
+        if isinstance(value, Module):
+            self._params[name] = value.parameters
+        elif isinstance(value, Parameter):
+            self._params[name] = [value]
+
+        super().__setattr__(name, value)
+
+    def __delattr__(self, name: str) -> None:
+        """Removes a given attribute from the module, and its parameters
+        if any.
+
+        :param name: The name of the attribute to be removed.
+        """
+        if name in self._params:
+            del self._params[name]
+
+        super().__delattr__(name)
+
+    @abstractmethod
+    def forward(self, *args: Any, **kwds: Any) -> Any:
+        """Runs the forward pass of the nnet.
+
+        :return: The output of the forward pass of the nnet.
+        """
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
